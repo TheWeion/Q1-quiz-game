e365577920d67cddbff0347c25cb34f5dec9604b
“TheWeion” <ianterryfallows@gmail.com>
2022-07-18 15:14:54 +0100

    Added Header and Footer layouts, fixed naming convention issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as MainMenu } from './MainMenu/MainMenu';
-export { default as Intro } from './Intro/Intro';
-export { default as GameSettings } from './GameSettings/GameSettings';
-export { default as Rules } from './Rules/Rules';
-export { default as Game } from './Game/Game';
-export { default as GameOver } from './GameOver/GameOver';
-export { default as Leaderboard } from './Leaderboard/Leaderboard';
+export { default as MainMenu } from './MainMenu';
+export { default as Intro } from './Intro';
+export { default as GameSettings } from './GameSettings';
+export { default as Rules } from './Rules';
+export { default as Game } from './Game';
+export { default as GameOver } from './GameOver';
+export { default as Leaderboard } from './Leaderboard';
