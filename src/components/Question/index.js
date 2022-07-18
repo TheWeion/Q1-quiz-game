4745adb958d2e980944c6f1794bfa3ce57ae1031
wingyuen2022 <wingyuenuk330@gmail.com>
2022-07-18 17:17:41 +0100

    Amended only correct answer can get the next page

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -16,35 +16,45 @@ const Question = ({playerId}) => {
         renderHTML();
     };
 
-    const prevQuestion = () => {
-        let player = targetPlayer;
-        if (player.lap > 0) {
-            player.lap = player.lap - 1;
-            dispatch(updatePlayer(player));
-            renderHTML();
-        }
-    };
-
     const renderHTML = () => {
         let html;
         let curQuestion = questions[targetPlayer.lap];
         if (curQuestion !== undefined && curQuestion !== null) {
             if (curQuestion !== undefined && curQuestion !== null) {
+                
                 html = `
-                <h1>${ curQuestion.category }</h1>
-                <h3>${ curQuestion.question }</h3>
-
-                <ul>
-                    <li key=${ Math.round(Math.random()*1000) }>${ curQuestion.correct_answer }</li>`;
-                    curQuestion.incorrect_answers.map((cur)=>{
-                        html = html + `<li key=${ Math.round(Math.random()*1000) }>${ cur }</li>`;
-                    });
-                    html = html + `</ul>`;
+                    <h1>${ curQuestion.category }</h1>
+                    <h3>${ curQuestion.question }</h3>
+                    <ul>`;
+                        let correct = curQuestion.correct_answer;
+                        let list = curQuestion.incorrect_answers;
+                        const positionInsertCorrectAnswer = Math.round(Math.random()*(curQuestion.incorrect_answers.length + 1));
+                        if (!list.includes(correct)) {
+                            list.splice(positionInsertCorrectAnswer, 0, correct);
+                        }
+                        list.map((cur, index)=>{
+                            html = html + `<li key=${ index }><button id="button_${ index }">${ cur }</button></li>`;
+                        });
+                html = html + `</ul>`;
+                const questionDiv = document.getElementById("question_content");
+                if (questionDiv !== undefined && questionDiv != null) {
+                    questionDiv.innerHTML = html;
+                    
+                }
+                list.map((cur, index)=>{
+                    const curButton = document.getElementById("button_" + index);
+                    if (curButton !== undefined && curButton !== null) {
+                        curButton.addEventListener('click', ()=>{
+                            if (cur === curQuestion.correct_answer) {
+                                alert("Correct");
+                                nextQuestion();
+                            } else {
+                                alert("Incorrect");
+                            }
+                        });
+                    }
+                });
             }
-        }
-        const questionDiv = document.getElementById("question_content");
-        if (questionDiv !== undefined && questionDiv != null) {
-            questionDiv.innerHTML = html;
         }
     };
 
@@ -57,8 +67,6 @@ const Question = ({playerId}) => {
             <div id="question_content">
                 { renderHTML() }    
             </div>
-            <button onClick={nextQuestion}>Next</button><br />
-            <button onClick={prevQuestion}>Back</button>
         </>
     );
 };
