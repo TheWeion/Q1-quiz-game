b9f5204afd7c1b857e71b69418d9ed1c775f9637
“TheWeion” <ianterryfallows@gmail.com>
2022-07-18 10:39:56 +0100

    Added Routes and MainMenu Page

---
+++ src/pages/MainMenu/MainMenu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+const MainMenu = () => {
+	  return (
+		<>
+			<img src={logo} className="App-logo" alt="logo" />
+
+			<div className="main-menu" >
+				<div className="frm-menu-container">
+					<MenuForm ></MenuForm>
+				</div>
+			</div>
+		</>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as MainMenu } from './MainMenu';
+export { default as Intro } from './Intro';
+export { default as GameSettings } from './GameSettings';
+export { default as Rules } from './Rules';
+export { default as Game } from './Game';
+export { default as GameOver } from './GameOver';
+export { default as Leaderboard } from './Leaderboard';
