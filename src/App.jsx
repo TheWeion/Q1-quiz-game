672b2707042e3c3997be37a15543da717afe92fb
“TheWeion” <ianterryfallows@gmail.com>
2022-07-18 12:22:52 +0100

    Added Rules Page, fixed routing defect.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom';
 import * as Pages from './pages';
 import './App.css';
-
 
 const App = () => {
   return (
@@ -17,26 +15,5 @@ const App = () => {
     </Routes>
   );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
