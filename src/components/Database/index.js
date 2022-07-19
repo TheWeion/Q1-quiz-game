955cb91f68413c5775aacf0432df722421e59ba2
Dakshitha Mak <dakshitha1992@hotmail.com>
2022-07-19 14:37:46 +0100

    added temp. database

---
+++ src/components/Database/index.js
@@ -0,0 +1,36 @@
+const UserScores = () => {
+    return ([
+        
+    {
+        id: 1,
+        name: "Aleks",
+        score: 30000
+    },
+    {
+        id: 2,
+        name: "Wing",
+        score: 29000
+    },
+    {
+        id: 3,
+        name: "Terry",
+        score: 27000
+    },
+    {
+        id: 4,
+        name: "Daks",
+        score: 22000
+    },
+    {
+        id: 5,
+        name: "Paul",
+        score: 23000
+    },
+    {
+        id: 6,
+        name: "Mitsy",
+        score: 24000
+    },
+])}
+
+export default UserScores;
