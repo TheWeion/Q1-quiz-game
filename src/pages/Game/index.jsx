9691070f0cb98de04e1809b7b67ed7118318cc1b
Aleopaa <aleksandraopacic@hotmail.co.uk>
2022-07-19 12:55:17 +0100

    message

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -5,18 +5,33 @@ import Timer from '../../components/Timer';
 import Radio from '../../components/Radio';
 // import QuizRender from '../../components/'
 import {useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
 
 const Game = () => {
 
 	const players = useSelector(state => state.playersReducer);
     const questions = useSelector(state => state.questionsReducer);
 
+	const [time, setTime] = useState(0);
+	const [running, setRunning] = useState(false);
+	
+	useEffect(() => {
+	  let interval;
+	  if (running) {
+		interval = setInterval(() => {
+		  setTime((prevTime) => prevTime + 10);
+		}, 10);
+	  } else if (!running) {
+		clearInterval(interval);
+	  }
+	  return () => clearInterval(interval);
+	}, [running]);
+
 	return (
 		<>
-			<Question  playerId={1} players = {players} questions = {questions}/>
-
-			<Timeline  playerId={1} players = {players} questions = {questions}/>
-      		<Timer  playerId={1} players = {players} questions = {questions} />
+			<Question  playerId={1} players = {players} questions = {questions}  />
+			<Timeline  playerId={1} players = {players} />
+      		<Timer  playerId={1} players = {players}  />
       		<Radio/>
 
 			{/* <ScoreBoard />
